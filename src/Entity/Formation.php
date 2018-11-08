29b075b6b552422c2ac246192c814be314581a62
vnans <adelph_85@hotmail.com>
2018-11-08 01:07:34 +0000

    neutrexint 1.1

---
+++ src/Entity/Formation.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FormationRepository")
+ */
+class Formation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $theme;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $soustheme;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $duree;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTheme(): ?string
+    {
+        return $this->theme;
+    }
+
+    public function setTheme(string $theme): self
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    public function getSoustheme(): ?string
+    {
+        return $this->soustheme;
+    }
+
+    public function setSoustheme(?string $soustheme): self
+    {
+        $this->soustheme = $soustheme;
+
+        return $this;
+    }
+
+    public function getDuree(): ?string
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(string $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+}
